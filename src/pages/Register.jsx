feace1dc6fdc9dfce473092abd9d792a1f006ea5
KnowMohit <mkg251412@gmail.com>
2024-10-16 22:58:16 +0530

    api added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://xenonstack-backend-qsm4.onrender.com/api/auth/register",
         {
           name,
           email,
